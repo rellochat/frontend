44a15edcc2b4c0a85abbab77a8bf3244753c059b
RT <rustytrim11@gmail.com>
2023-08-29 19:23:31 -0400

    Layout setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useEffect } from 'react';
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Rello',
+  description: 'A chatting application for you and your fellow friends!',
+  keywords: ["chat"],
+  themeColor: "#E0205A",
+  viewport: "width=device-width, initial-scale=1",
+  creator: "Rello Team"
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  useEffect(() => {
+    window.addEventListener("contextmenu", (event) => event.preventDefault());
+    return () => window.removeEventListener("contextmenu", (event) => event.preventDefault());
+  }, []);
+
+  return (
+    <html lang="en">
+      <body className="bg-primary text-primary">{children}</body>
+    </html>
+  )
+}
