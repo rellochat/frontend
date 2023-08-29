a86642137c89a2b0d2a4d939a72cf3cc0df87020
RT <rustytrim11@gmail.com>
2023-08-29 19:29:07 -0400

    Move NavLink to util dir in components dir

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import NavLink from "./NavLink";
+import NavLink from "./util/NavLink";
 
 export default function Navbar() {
     return (
