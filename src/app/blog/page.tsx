ad0be0e6158bf415d9620a37e09e316afa3c24f5
RT <rustytrim11@gmail.com>
2023-08-29 19:24:06 -0400

    Added new page routes

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar";
+
+export default function Blog() {
+  return (
+    <div className='w-screen h-screen'>
+      <Navbar/>
+    </div>
+  )
+}
---
+++ src/app/support/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar";
+
+export default function Support() {
+  return (
+    <div className='w-screen h-screen'>
+      <Navbar/>
+    </div>
+  )
+}
