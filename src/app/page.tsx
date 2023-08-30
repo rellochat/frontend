e12ad08b153f427607a37eef24a9eda1602b1800
RT <rustytrim11@gmail.com>
2023-08-29 20:32:54 -0400

    Navbody for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import NavBody from "@/components/NavBody";
 import Navbar from "@/components/Navbar";
+import NavButton from "@/components/util/NavButton";
 
 export default function Home() {
   return (
     <div className='w-screen h-screen'>
-      <Navbar/>
+      <Navbar />
+      <NavBody>
+        <div className="w-full h-full flex justify-center items-center gap-4">
+          <NavButton href="/download">Download</NavButton>
+          <NavButton href="/login">Open Web App</NavButton>
+        </div>
+      </NavBody>
     </div>
   )
 }
