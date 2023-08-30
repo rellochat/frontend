73d996f26e4ba2a84ce089c77b01327b03b277e7
RT <rustytrim11@gmail.com>
2023-08-29 20:32:36 -0400

    Logo redirects to home and link row has gaps

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import Link from "next/link";
 import NavLink from "./util/NavLink";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
         <div className="w-full h-landing-header flex justify-between items-center px-3 sm:px-12">
-            <div>
+            <Link href={"/"}>
                 <Image src={"/landing-banner.png"} draggable={false} width={300} height={60} alt="banner" className="hidden sm:block" />
                 <Image src={"/logo.png"} draggable={false} width={60} height={60} alt="logo" className="block sm:hidden" />
-            </div>
-            <nav className="flex items-center">
+            </Link>
+            <nav className="flex items-center gap-4">
                 <NavLink href="/support" className="text-accent-primary">Support</NavLink>
                 <NavLink href="/blog" className="text-accent-primary">Blog</NavLink>
                 <NavLink href="/login" className="text-accent-primary">Login</NavLink>
