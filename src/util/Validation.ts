0efa24f71781621c85cbcef3635f0ee9cfe7353a
RT <rustytrim11@gmail.com>
2023-09-01 21:55:21 -0400

    Email and password validation

---
+++ src/util/Validation.ts
@@ -0,0 +1,18 @@
+export default class Validation {
+
+    public static email(email: string | null) {
+        if (!email) return { code: 401, message: "This field is required." };
+        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) return { code: 401, message: "Your email must be a proper email." };
+        return { code: 200, message: null };
+    }
+
+    public static password(password: string | null) {
+        if (!password) return { code: 401, message: "This field is required." };
+        if (!/^.{8,}$/.test(password)) return { code: 401, message: "The password must be atleast 8 characters long." };
+        if (!/[A-Z]/.test(password)) return { code: 401, message: "The password must contain atleast one capital letter (A-Z)." };
+        if (!/[a-z]/.test(password)) return { code: 401, message: "The password must contain atleast a lowercase letter (a-z)." };
+        if (!/[!@#$%^&*]/.test(password)) return { code: 401, message: "The password must include atleast one special character (!, @, #, $, %, ^, &, *)." };
+
+        return { code: 200, message: null };
+    }
+}
