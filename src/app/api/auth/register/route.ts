f2838ae7e5875b32e00e673a07a7e1780c124ff6
RT <rustytrim11@gmail.com>
2023-09-02 04:36:38 -0400

    Login and Register api updates

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -8,15 +8,15 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI!);
 
 export async function POST(request: Request) {
-    const { email, password } = await request.json();
+    const { email, password, confirmPassword } = await request.json();
 
     const emailValidation = Validation.email(email);
-    const passwordValidation = Validation.password(password);
+    const passwordValidation = Validation.password(password, true);
 
     if (emailValidation.code !== 200) return NextResponse.json(emailValidation);
     if (passwordValidation.code !== 200) return NextResponse.json(passwordValidation);
 
-    await mongoose.disconnect();
-    
+    if (!confirmPassword || confirmPassword !== password) return NextResponse.json({ code: 401, message: "Passwords must match." });
+
     return NextResponse.json({ message: "Hello World!" });
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,20 @@ import NavBody from "@/components/NavBody";
 import Navbar from "@/components/Navbar";
 import Button from "@/components/util/Button";
 import Link from "next/link";
+import { FormEvent, useState } from "react";
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        fetch("/api/auth/login", { method: "POST", body: JSON.stringify({ email, password }) }).then(async (res) => {
+            console.log(await res.json());
+        })
+    }
+
     return (
         <div className='w-screen h-screen'>
             <div className="sm:block hidden">
@@ -15,17 +27,16 @@ export default function Login() {
                     <div className="w-full h-full sm:w-[500px] sm:h-[360px] bg-secondary sm:rounded-xl px-4 py-2">
                         <h1 className="text-center">Login</h1>
                         <p className="text-center text-secondary font-bold">Login to start chatting!</p>
-                        <div className="w-full h-fit flex flex-col items-center mt-4">
+                        <form className="w-full h-fit flex flex-col items-center mt-4" onSubmit={(event) => handleSubmit(event)}>
                             <label className="font-bold text-secondary w-2/3">EMAIL</label>
-                            <input placeholder="Enter your email here" className="input w-2/3 px-2 py-1.5 outline-none rounded-md" />
+                            <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter your email here" className="input w-2/3 px-2 py-1.5 outline-none rounded-md" />
                             <div className="w-full h-5" />
                             <label className="font-bold text-secondary w-2/3">PASSWORD</label>
-                            <input placeholder="********" className="input w-2/3 px-2 py-1.5 outline-none rounded-md" />
-                            {/* <button className="accent-primary w-1/3 p-2 rounded-md mt-4">Login</button> */}
-                            <Button onClick={() => console.log("Hello!")} className="accent-primary w-1/3 rounded-md mt-4">Login</Button>
+                            <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="********" className="input w-2/3 px-2 py-1.5 outline-none rounded-md" />
+                            <Button className="accent-primary w-1/3 rounded-md mt-4">Login</Button>
                             <Link className="font-bold link w-fit text-sm mt-3" href={"/register"}>New to Rello?</Link>
                             <Link className="font-bold link w-fit text-sm mt-0.5" href={"/forgot-pass"}>Forgot your password?</Link>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </NavBody>
